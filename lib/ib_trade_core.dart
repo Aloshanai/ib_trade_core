57bc505857d0871a465b1c8bfbe6accf44000132
sammy-techie <sam689422@gmail.com>
2026-07-10 19:52:00 +0530

    Initialize ib_trade_core Dart package template

---
+++ lib/ib_trade_core.dart
@@ -0,0 +1,7 @@
+/// Core foundation for Interactive Brokers (IBKR) Dart client SDK.
+library;
+
+// Export files from src/ when they are implemented:
+// export 'src/client/cookie_client.dart';
+// export 'src/client/http_client.dart';
+// export 'src/models/models.dart';
